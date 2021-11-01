a51f35586190473a304c66b21c404046a0e11c54
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-11-01 11:55:44 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+from config import bot
+from pyrogram.raw.types import UpdateGroupCallParticipants
+from pyrogram.raw.types import UpdateGroupCallParticipants, PeerChannel
+from pyrogram.raw.functions.channels import EditBanned
+from pyrogram.raw.types import InputPeerChannel, InputChannel, ChatBannedRights, InputGroupCall
+
+
+@bot.on_raw_update()
+async def hemheupdet(client, update, users, chats):
+    banchat = await client.resolve_peer("DecodeSupport")
+    if isinstance(update, UpdateGroupCallParticipants):
+        for x in update.participants:
+            if isinstance(x.peer, PeerChannel):
+                try:
+                    hehe = await client.resolve_peer(int(str(-100) + str(x.peer.channel_id)))
+                    await client.send(EditBanned(channel=banchat, participant=hehe, banned_rights=ChatBannedRights(until_date=0,
+                                view_messages=True,
+                                send_messages=True,
+                                send_media=True,
+                                send_stickers=True,
+                                send_gifs=True,
+                                send_games=True,
+                                send_inline=True,
+                                embed_links=True)))
+                    await client.send_message(chat, f"Successfully Banned {int(str(-100) + str(x.peer.channel_id))}")
+
+                except Exception as e:
+                    await client.send_message(chat, f"{e} \n\n{x.peer}")
