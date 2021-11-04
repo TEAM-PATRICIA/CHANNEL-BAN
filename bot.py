e53af01e9d3c6d7143de5a7854d78b502c274634
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-11-05 00:43:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from config import Config
-from config import Chat as chat
+from config import CHAT as chat
 from pyrogram import Client, idle
 from pyrogram.raw.types import UpdateGroupCallParticipants
 from pyrogram.raw.types import UpdateGroupCallParticipants, PeerChannel
