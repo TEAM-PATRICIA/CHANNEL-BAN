b086ac78abc81d8265a35eef32a120b1ca7a3006
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-11-06 07:50:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram import Client, idle
 from pyrogram.raw.types import UpdateGroupCallParticipants, PeerChannel
 from pyrogram.raw.functions.channels import EditBanned
 from pyrogram.raw.types import InputPeerChannel, InputChannel, ChatBannedRights, InputGroupCall
-from pyrogram.raw.functions.phone import EditGroupCallParticipant
+from pyrogram.raw.functions.phone import EditGroupCallParticipant, GetGroupParticipants
 
 chat=Config.CHAT
 api_id=Config.API_ID
@@ -14,10 +14,10 @@ bot = Client(session_name, api_id, api_hash)
 @bot.on_raw_update()
 async def hemheupdet(client, update, users, chats):
     banchat = await client.resolve_peer("DecodeSupport")
-    if isinstance(update, UpdateGroupCallParticipants):
+    if isinstance(update, GetGroupParticipants):
         for x in update.participants:
             if isinstance(x.peer, PeerChannel):
-                    await client.EditGroupCallParticipant(call=update.call, participant=x.peer.channel_id, muted=True)
+                    await client.send(EditGroupCallParticipant(call=update.call, participant=x.peer.channel_id, muted=True))
                     await client.kick_chat_member("DecodeSupport", x.peer.channel_id)
                     await client.send_message(chat, f"Successfully Banned str(x.peer.channel_id)")
                     await client.send_message(chat, f"{e} \n\n{x.peer}")
